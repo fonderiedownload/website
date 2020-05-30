bcd397a32e428e4154eae8df77d82bd55a6c8a87
Romain Pellerin <rpellerin@users.noreply.github.com>
2020-05-30 19:09:40 +0200

    Add scripts

---
+++ scripts/buildAssets.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const path = require("path");
+
+const PICTURES_DIRECTORY = path.join(__dirname, "..", "inuse");
+
+const OUTPUT_FILE = path.join(__dirname, "..", "assets.js");
+
+const buildAssets = (array) => array;
+// TODO newest first .sort((a, b) => a.hash - b.hash)
+
+const images = fs
+  .readdirSync(PICTURES_DIRECTORY)
+  .filter((file) => file.indexOf(".") !== 0)
+  .map((file) => path.join(".", "inuse", file));
+
+const assets = buildAssets(images);
+
+const outputFileContent =
+  "window.images = " + JSON.stringify(assets, null, "\t");
+
+fs.writeFile(OUTPUT_FILE, outputFileContent, "utf8", (err) => {
+  if (err) throw err;
+  console.log("The file has been saved!");
+});
