f27760dfead32b56af9a3626d6c540cca760b38b
Romain Pellerin <rpellerin@users.noreply.github.com>
2020-06-15 22:52:17 +0200

    Add colcade

--- assets.js
+++ assets.js
@@ -1,4 +1,14 @@
 window.images = [
-	"inuse/fictionalposterparThomasCarlotti.jpeg",
-	"inuse/revuePD-02-13parArielMartínPérez.jpg"
+	"inuse/1280px-Vegvisir.svg.png",
+	"inuse/19840595-005.jpg",
+	"inuse/20424040_1407574959355765_2595975017749929911_o.jpg",
+	"inuse/Capture d’écran 2019-05-08 à 10.33.54.png",
+	"inuse/Capture d’écran 2020-02-22 à 22.53.44.png",
+	"inuse/LaGousse28.jpg",
+	"inuse/STICKER_COVID.png",
+	"inuse/eta22032006.jpg",
+	"inuse/fictionalposterparThomasCarlotti.jpg",
+	"inuse/istockphoto-637727798-612x612.jpg",
+	"inuse/revuePD-02-13parArielMartínPérez.jpg",
+	"inuse/tumblr_p140xo7F8z1vy5e6do1_r1_640.jpg"
 ]
