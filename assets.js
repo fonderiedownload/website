c32fa8b22986295a9cbaa909700253f3f69b4e3d
Romain Pellerin <rpellerin@users.noreply.github.com>
2021-02-24 22:00:39 +0100

    Fix tag names

--- assets.js
+++ assets.js
@@ -1,49 +1,179 @@
 window.images = [
 	{
-		"path": "inuse/012.jpg"
-	},
-	{
-		"path": "inuse/011.png"
-	},
-	{
-		"path": "inuse/010.jpg"
-	},
-	{
-		"path": "inuse/009.jpg"
-	},
-	{
-		"path": "inuse/008.jpg"
-	},
-	{
-		"path": "inuse/007.jpg"
-	},
-	{
-		"path": "inuse/006.jpg"
-	},
-	{
-		"path": "inuse/005.png"
-	},
-	{
-		"path": "inuse/004-leo-art.png"
-	},
-	{
-		"path": "inuse/003.jpg"
-	},
-	{
-		"path": "inuse/002-yolo.jpg",
+		"path": "inuse/022.jpg",
 		"tags": [
-			"garamondt",
-			"zara"
+			"Zara"
 		],
-		"title": "Random pic",
-		"author": "Van Gogh"
+		"title": "Next Cloud Residency",
+		"author": "Sofia Braga"
 	},
 	{
-		"path": "inuse/001.png",
+		"path": "inuse/021.jpg",
 		"tags": [
-			"zara"
+			"Garamon(d/t)"
 		],
-		"title": "My beautiful picture",
-		"author": "Picasso"
+		"title": "Is graphic design too trendy? It's Nice That",
+		"author": "Elsa Müller"
+	},
+	{
+		"path": "inuse/020.jpg",
+		"tags": [
+			"Garamon(d/t)"
+		],
+		"title": "Journée portes ouvertes de l’ENSAD Nancy",
+		"author": "Jimmy Cintero, Jules Durand, Mariel Nils"
+	},
+	{
+		"path": "inuse/019.jpg",
+		"tags": [
+			"Garamon(d/t)"
+		],
+		"title": "Journée portes ouvertes de l’ENSAD Nancy",
+		"author": "Jimmy Cintero, Jules Durand, Mariel Nils"
+	},
+	{
+		"path": "inuse/018.jpg",
+		"tags": [
+			"Garamon(d/t)"
+		],
+		"title": "Journée portes ouvertes de l’ENSAD Nancy",
+		"author": "Jimmy Cintero, Jules Durand, Mariel Nils"
+	},
+	{
+		"path": "inuse/017.jpg",
+		"tags": [
+			"4 fromages"
+		],
+		"title": "Plasma magazine n°5",
+		"author": "Studio Laurens Bauer & David Benski (photographed by Nora Heinisch)"
+	},
+	{
+		"path": "inuse/016.jpg",
+		"tags": [
+			"4 fromages"
+		],
+		"title": "Plasma magazine n°5",
+		"author": "Studio Laurens Bauer & David Benski (photographed by Nora Heinisch)"
+	},
+	{
+		"path": "inuse/015.jpg",
+		"tags": [
+			"4 fromages"
+		],
+		"title": "Plasma magazine n°5",
+		"author": "Studio Laurens Bauer & David Benski"
+	},
+	{
+		"path": "inuse/014.jpg",
+		"tags": [
+			"4 fromages"
+		],
+		"title": "Plasma magazine n°5",
+		"author": "Studio Laurens Bauer & David Benski"
+	},
+	{
+		"path": "inuse/013.jpg",
+		"tags": [
+			"4 fromages"
+		],
+		"title": "Plasma magazine n°5",
+		"author": "Studio Laurens Bauer & David Benski"
+	},
+	{
+		"path": "inuse/012.jpg",
+		"tags": [
+			"4 fromages"
+		],
+		"title": "Plasma magazine n°5",
+		"author": "Studio Laurens Bauer & David Benski"
+	},
+	{
+		"path": "inuse/011.jpg",
+		"tags": [
+			"Garamon(d/t)"
+		],
+		"title": "Enter Promocode",
+		"author": "Sofia Braga"
+	},
+	{
+		"path": "inuse/010.jpg",
+		"tags": [
+			"Garamon(d/t)"
+		],
+		"title": "Enter Promocode",
+		"author": "Sofia Braga"
+	},
+	{
+		"path": "inuse/009.jpg",
+		"tags": [
+			"Garamon(d/t)"
+		],
+		"title": "Enter Promocode",
+		"author": "Sofia Braga"
+	},
+	{
+		"path": "inuse/008.jpg",
+		"tags": [
+			"Garamon(d/t)"
+		],
+		"title": "Sartorial",
+		"author": "Marcus DuBois"
+	},
+	{
+		"path": "inuse/007.jpg",
+		"tags": [
+			"Garamon(d/t)"
+		],
+		"title": "To Sustain the Vision",
+		"author": "Roxanne Maillet"
+	},
+	{
+		"path": "inuse/006.jpg",
+		"tags": [
+			"Garamon(d/t)"
+		],
+		"title": "Andrea Crews jean custom workshop",
+		"author": "Anouk Jeanningros, Léonie Montjarret"
+	},
+	{
+		"path": "inuse/005.jpg",
+		"tags": [
+			"Tribalium"
+		],
+		"title": "Iceberg",
+		"author": "Eirveibei"
+	},
+	{
+		"path": "inuse/004.jpg",
+		"tags": [
+			"Quarantype"
+		],
+		"title": "Vinyl Party",
+		"author": "Alexis Faudot"
+	},
+	{
+		"path": "inuse/003.jpg",
+		"tags": [
+			"Tribalium"
+		],
+		"title": "La Gousse #28",
+		"author": "Cécile Bouffard, Roxanne Maillet, Barbara Quintin"
+	},
+	{
+		"path": "inuse/002.jpg",
+		"tags": [
+			"Garamon(d/t)"
+		],
+		"title": "Fictional poster",
+		"author": "Thomas Carlotti"
+	},
+	{
+		"path": "inuse/001.jpg",
+		"tags": [
+			"Crédible",
+			"Zara"
+		],
+		"title": "PD La Revue",
+		"author": "Ariel Martín Pérez"
 	}
 ]
