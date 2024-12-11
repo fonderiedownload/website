e7beca60e596a5191499d827c239390d496f5bd8
GitHub Action <action@github.com>
2024-12-11 11:33:12 +0000

    Release-2024-12-11T11:33:11+00:00

--- assets.js
+++ assets.js
@@ -1,5 +1,645 @@
 window.images = [
 	{
+		"path": "inuse/102.jpg",
+		"tags": [
+			"Crédible"
+		],
+		"title": "R.I.P La CYB Halloween Party",
+		"author": "Cyberrance"
+	},
+	{
+		"path": "inuse/101.jpg",
+		"tags": [
+			"4 fromages"
+		],
+		"title": "Garage Mu festival",
+		"author": "Félicité Landrivon a.k.a. Brigade Cynophile"
+	},
+	{
+		"path": "inuse/100.jpg",
+		"tags": [
+			"Zara"
+		],
+		"title": "Exercices de storyboard",
+		"author": "Maurice Nyssen"
+	},
+	{
+		"path": "inuse/099.jpg",
+		"tags": [
+			"Zara"
+		],
+		"title": "Exercices de storyboard",
+		"author": "Maurice Nyssen"
+	},
+	{
+		"path": "inuse/098.jpg",
+		"tags": [
+			"Garamon(d/t)"
+		],
+		"title": "(Social) Fermentation",
+		"author": "Andromachi Theodoridou, Sarah Traub and Kirsten Humpfer"
+	},
+	{
+		"path": "inuse/097.jpg",
+		"tags": [
+			"Garamon(d/t)"
+		],
+		"title": "(Social) Fermentation",
+		"author": "Andromachi Theodoridou, Sarah Traub and Kirsten Humpfer"
+	},
+	{
+		"path": "inuse/096.jpg",
+		"tags": [
+			"Crédible"
+		],
+		"title": "(Social) Fermentation",
+		"author": "Andromachi Theodoridou, Sarah Traub and Kirsten Humpfer"
+	},
+	{
+		"path": "inuse/095.jpg",
+		"tags": [
+			"Crédible Garamon(d/t)"
+		],
+		"title": "Hoera concert at Het Bos Antwerpen",
+		"author": "Victor Verhelst"
+	},
+	{
+		"path": "inuse/094.jpg",
+		"tags": [
+			"Crédible"
+		],
+		"title": "Cringe Académie - Léopold Bensaid",
+		"author": "L'Articho éditions"
+	},
+	{
+		"path": "inuse/093.jpg",
+		"tags": [
+			"Tribalium"
+		],
+		"title": "3AM voice notes",
+		"author": "3AM voice notes"
+	},
+	{
+		"path": "inuse/092.jpg",
+		"tags": [
+			"Zara"
+		],
+		"title": "Leaflet for Kristof Van Gestel",
+		"author": "Lien Van Leemput"
+	},
+	{
+		"path": "inuse/091.jpg",
+		"tags": [
+			"Zara"
+		],
+		"title": "Leaflet for Kristof Van Gestel",
+		"author": "Lien Van Leemput"
+	},
+	{
+		"path": "inuse/090.jpg",
+		"tags": [
+			"Crédible"
+		],
+		"title": "Napoli - photo book by Vincen Beeckman",
+		"author": "Lien Van Leemput"
+	},
+	{
+		"path": "inuse/089.jpg",
+		"tags": [
+			"4 fromages"
+		],
+		"title": "MetroGoldia",
+		"author": "MetroGoldia"
+	},
+	{
+		"path": "inuse/088.jpg",
+		"tags": [
+			"4 fromages"
+		],
+		"title": "Jadey",
+		"author": "Charley McDermott-Edwards"
+	},
+	{
+		"path": "inuse/087.jpg",
+		"tags": [
+			"Crédible"
+		],
+		"title": "Isengard party at Brasserie Illegal",
+		"author": "Édouard Nazé"
+	},
+	{
+		"path": "inuse/086.jpg",
+		"tags": [
+			"Crédible"
+		],
+		"title": "Paris cortège travailleureuses de l'art",
+		"author": "Syndicat National des Artistes Plasticien·nes de la CGT"
+	},
+	{
+		"path": "inuse/085.jpg",
+		"tags": [
+			"4 fromages"
+		],
+		"title": "ESAC FC Paninini poster",
+		"author": "Jérémy Breton"
+	},
+	{
+		"path": "inuse/084.jpg",
+		"tags": [
+			"Garamon(d/t)"
+		],
+		"title": "Sarjakuvakeskus Lasten Lauantai - Tehdään kirja!",
+		"author": "The Temporary Bookshelf"
+	},
+	{
+		"path": "inuse/083.jpg",
+		"tags": [
+			"Crédible"
+		],
+		"title": "Schiev Festival",
+		"author": "Alliage, Philippine Bordeaux, Édouard Nazé, Baptiste Poligné et Clément Horard"
+	},
+	{
+		"path": "inuse/082.jpg",
+		"tags": [
+			"Garamon(d/t)"
+		],
+		"title": "Venezia Fabrica Futura - An eco-critical lab",
+		"author": "Stefano Dealessandri"
+	},
+	{
+		"path": "inuse/081.jpg",
+		"tags": [
+			"Garamon(d/t)"
+		],
+		"title": "Venezia Fabrica Futura - An eco-critical lab",
+		"author": "Stefano Dealessandri"
+	},
+	{
+		"path": "inuse/080.jpg",
+		"tags": [
+			"Tribalium"
+		],
+		"title": "Carly Zeng - Mini Asia Tour",
+		"author": "Studio Cryo"
+	},
+	{
+		"path": "inuse/079.jpg",
+		"tags": [
+			"Crédible Garamon(d/t)"
+		],
+		"title": "XPUB Special Issue 15 - Radio Implicancies",
+		"author": "2021 XPUB master's students"
+	},
+	{
+		"path": "inuse/078.jpg",
+		"tags": [
+			"Garamon(d/t)"
+		],
+		"title": "Le bestiaire de Guillaume Apollinaire",
+		"author": "Teddy Chantharath"
+	},
+	{
+		"path": "inuse/077.jpg",
+		"tags": [
+			"Garamon(d/t)"
+		],
+		"title": "Un Fanzine par Mois",
+		"author": "Jules Tirilly"
+	},
+	{
+		"path": "inuse/076.jpg",
+		"tags": [
+			"Quarantype Garamon(d/t)"
+		],
+		"title": "Subjunctive Porosity",
+		"author": "Xuanjie Huang"
+	},
+	{
+		"path": "inuse/075.jpg",
+		"tags": [
+			"Garamon(d/t)"
+		],
+		"title": "Subjunctive Porosity",
+		"author": "Xuanjie Huang"
+	},
+	{
+		"path": "inuse/074.jpg",
+		"tags": [
+			"Garamon(d/t)"
+		],
+		"title": "Mariachi / Ursatz / Elvin Brandhi",
+		"author": "Collectif La Méandre"
+	},
+	{
+		"path": "inuse/073.jpg",
+		"tags": [
+			"Garamon(d/t)"
+		],
+		"title": "MONUMENTA MORI - Création textuelle",
+		"author": "erg"
+	},
+	{
+		"path": "inuse/072.jpg",
+		"tags": [
+			"Tribalium Garamon(d/t)"
+		],
+		"title": "A concert at Pracht in Leipzig",
+		"author": "Carla Selva Matthes"
+	},
+	{
+		"path": "inuse/071.jpg",
+		"tags": [
+			"Crédible"
+		],
+		"title": "Coraline",
+		"author": "WhiteWalka Beats"
+	},
+	{
+		"path": "inuse/070.jpg",
+		"tags": [
+			"Tribalium"
+		],
+		"title": "Les Cheveux mouillés",
+		"author": "Andréas Camps"
+	},
+	{
+		"path": "inuse/069.jpg",
+		"tags": [
+			"Tribalium"
+		],
+		"title": "Les Cheveux mouillés",
+		"author": "Andréas Camps"
+	},
+	{
+		"path": "inuse/068.jpg",
+		"tags": [
+			"Garamon(d/t)"
+		],
+		"title": "Les Bacchanales du comité informel",
+		"author": "Laurie Giraud and Geoffrey Bourgeois"
+	},
+	{
+		"path": "inuse/067.jpg",
+		"tags": [
+			"Garamon(d/t)"
+		],
+		"title": "Les Bacchanales du comité informel",
+		"author": "Laurie Giraud and Geoffrey Bourgeois"
+	},
+	{
+		"path": "inuse/066.jpg",
+		"tags": [
+			"Crédible"
+		],
+		"title": "Not Great Not Terrible Hoodie",
+		"author": "Anton Abo"
+	},
+	{
+		"path": "inuse/065.jpg",
+		"tags": [
+			"Garamon(d/t)"
+		],
+		"title": "Numérique, génétique, robotique vers la transition écologique ?",
+		"author": "L’Amicale du Futur"
+	},
+	{
+		"path": "inuse/064.jpg",
+		"tags": [
+			"Crédible"
+		],
+		"title": "MARICAS Records 002 Sixty 9 / Triple D",
+		"author": "Gina Guasch Team"
+	},
+	{
+		"path": "inuse/063.jpg",
+		"tags": [
+			"Crédible"
+		],
+		"title": "MARICAS Records 002 Sixty 9 / Triple D",
+		"author": "Gina Guasch Team"
+	},
+	{
+		"path": "inuse/062.jpg",
+		"tags": [
+			"Crédible"
+		],
+		"title": "Happy New Year Poster",
+		"author": "Donil Lavashov"
+	},
+	{
+		"path": "inuse/061.jpg",
+		"tags": [
+			"4 fromages"
+		],
+		"title": "HST 2022",
+		"author": "Liudmila Savelyeva"
+	},
+	{
+		"path": "inuse/060.jpg",
+		"tags": [
+			"Garamon(d/t)"
+		],
+		"title": "Juicy Workshops",
+		"author": "Liudmila Savelyeva"
+	},
+	{
+		"path": "inuse/059.jpg",
+		"tags": [
+			"Garamon(d/t)"
+		],
+		"title": "La Maison du jardinier",
+		"author": ""
+	},
+	{
+		"path": "inuse/058.jpg",
+		"tags": [
+			"Garamon(d/t)"
+		],
+		"title": "Sous peu il fera jour",
+		"author": "Alexis Faudot and Léo Gauttier"
+	},
+	{
+		"path": "inuse/057.jpg",
+		"tags": [
+			"Garamon(d/t)"
+		],
+		"title": "Nothing New Under the Sun",
+		"author": "Katri Astala (photo by Anna Niskanen)"
+	},
+	{
+		"path": "inuse/056.jpg",
+		"tags": [
+			"Garamon(d/t)"
+		],
+		"title": "Nothing New Under the Sun",
+		"author": "Katri Astala (photo by Anna Niskanen)"
+	},
+	{
+		"path": "inuse/055.jpg",
+		"tags": [
+			"Garamon(d/t)"
+		],
+		"title": "Nothing New Under the Sun",
+		"author": "Katri Astala"
+	},
+	{
+		"path": "inuse/054.jpg",
+		"tags": [
+			"Crédible"
+		],
+		"title": "Isengard Guilde Meet Sahara Hardcore",
+		"author": "Jules Rousselet and Paul Peyrolle"
+	},
+	{
+		"path": "inuse/053.jpg",
+		"tags": [
+			"Crédible"
+		],
+		"title": "Le Ivre en pierre",
+		"author": "Jules Rousselet"
+	},
+	{
+		"path": "inuse/052.jpg",
+		"tags": [
+			"Crédible"
+		],
+		"title": "Ventoline en tournée",
+		"author": "Félicité Landrivon a.k.a. Brigade Cynophile (drawing by Līva Kandevica)"
+	},
+	{
+		"path": "inuse/051.jpg",
+		"tags": [
+			"Crédible"
+		],
+		"title": "Ventoline 4",
+		"author": "Félicité Landrivon a.k.a. Brigade Cynophile (drawing by Līva Kandevica)"
+	},
+	{
+		"path": "inuse/050.jpg",
+		"tags": [
+			"Crédible"
+		],
+		"title": "Lightning Bolt, Container and Cube gig at Grnd Zero",
+		"author": "Félicité Landrivon a.k.a. Brigade Cynophile"
+	},
+	{
+		"path": "inuse/049.jpg",
+		"tags": [
+			"Crédible"
+		],
+		"title": "Mosquito News",
+		"author": "Jaan Pavliuk"
+	},
+	{
+		"path": "inuse/048.jpg",
+		"tags": [
+			"Tribalium"
+		],
+		"title": "Born a loser",
+		"author": "Simon Renoud-Grappin"
+	},
+	{
+		"path": "inuse/047.jpg",
+		"tags": [
+			"Garamon(d/t)"
+		],
+		"title": "Tu te fourres le doigt dans l'œil",
+		"author": "Jennifer Lemoy"
+	},
+	{
+		"path": "inuse/046.jpg",
+		"tags": [
+			"Tribalium"
+		],
+		"title": "Review of the Year 2021 - Photography top 25",
+		"author": "It's Nice That"
+	},
+	{
+		"path": "inuse/045.jpg",
+		"tags": [
+			"Crédible"
+		],
+		"title": "Stuart Hall - The Work of representation, Chapter 3",
+		"author": "Aliona Ciobanu, Nova Lie and Stefania Bodnia"
+	},
+	{
+		"path": "inuse/044.jpg",
+		"tags": [
+			"Crédible"
+		],
+		"title": "Stuart Hall - The Work of representation, Chapter 3",
+		"author": "Aliona Ciobanu, Nova Lie and Stefania Bodnia"
+	},
+	{
+		"path": "inuse/043.jpg",
+		"tags": [
+			"Crédible"
+		],
+		"title": "Stuart Hall - The Work of representation, Chapter 3",
+		"author": "Aliona Ciobanu, Nova Lie and Stefania Bodnia"
+	},
+	{
+		"path": "inuse/042.jpg",
+		"tags": [
+			"Crédible"
+		],
+		"title": "Stuart Hall - The Work of representation, Chapter 3",
+		"author": "Aliona Ciobanu, Nova Lie and Stefania Bodnia"
+	},
+	{
+		"path": "inuse/041.jpg",
+		"tags": [
+			"4 fromages"
+		],
+		"title": "Faut raver - Tustance festival",
+		"author": "Bortkletlend"
+	},
+	{
+		"path": "inuse/040.jpg",
+		"tags": [
+			"Garamon(d/t)"
+		],
+		"title": "Imagination Station",
+		"author": "Matt Vlach"
+	},
+	{
+		"path": "inuse/039.jpg",
+		"tags": [
+			"4 fromages"
+		],
+		"title": "unauthorized advertising for fonderie.download",
+		"author": "Unknown"
+	},
+	{
+		"path": "inuse/038.jpg",
+		"tags": [
+			"Garamon(d/t)"
+		],
+		"title": "Letters From a Foreign Mind",
+		"author": "Kersti Heile"
+	},
+	{
+		"path": "inuse/037.jpg",
+		"tags": [
+			"Garamon(d/t)"
+		],
+		"title": "Letters From a Foreign Mind",
+		"author": "Kersti Heile"
+	},
+	{
+		"path": "inuse/036.jpg",
+		"tags": [
+			"Garamon(d/t)"
+		],
+		"title": "Letters From a Foreign Mind",
+		"author": "Kersti Heile"
+	},
+	{
+		"path": "inuse/035.jpg",
+		"tags": [
+			"Garamon(d/t)"
+		],
+		"title": "Letters From a Foreign Mind",
+		"author": "Kersti Heile"
+	},
+	{
+		"path": "inuse/034.jpg",
+		"tags": [
+			"Garamon(d/t)"
+		],
+		"title": "Letters From a Foreign Mind",
+		"author": "Kersti Heile"
+	},
+	{
+		"path": "inuse/033.jpg",
+		"tags": [
+			"Garamon(d/t)"
+		],
+		"title": "Hard Dance 122",
+		"author": "Boiler Room"
+	},
+	{
+		"path": "inuse/032.jpg",
+		"tags": [
+			"Zara"
+		],
+		"title": "Poltergeist",
+		"author": "Clear Mind Developers"
+	},
+	{
+		"path": "inuse/031.jpg",
+		"tags": [
+			"Zara"
+		],
+		"title": "Poltergeist",
+		"author": "Clear Mind Developers"
+	},
+	{
+		"path": "inuse/030.jpg",
+		"tags": [
+			"Zara"
+		],
+		"title": "Poltergeist",
+		"author": "Clear Mind Developers"
+	},
+	{
+		"path": "inuse/029.jpg",
+		"tags": [
+			"Quarantype"
+		],
+		"title": "smart systems",
+		"author": "Anton Abo"
+	},
+	{
+		"path": "inuse/028.jpg",
+		"tags": [
+			"Garamon(d/t)"
+		],
+		"title": "Le soleil est noir",
+		"author": "Léa Laforest"
+	},
+	{
+		"path": "inuse/027.jpg",
+		"tags": [
+			"4 fromages"
+		],
+		"title": "Plasma magazine n°5",
+		"author": "Studio Laurens Bauer & David Benski (photographed by Nora Heinisch)"
+	},
+	{
+		"path": "inuse/026.jpg",
+		"tags": [
+			"Quarantype"
+		],
+		"title": "PD La Revue n°3",
+		"author": "Ariel Martín Pérez (with a beautiful illustration by Aubrey Beardsley)"
+	},
+	{
+		"path": "inuse/025.png",
+		"tags": [
+			"Crédible"
+		],
+		"title": "Next Generation survey",
+		"author": "It's Nice That"
+	},
+	{
+		"path": "inuse/024.jpg",
+		"tags": [
+			"Crédible"
+		],
+		"title": "Next Generation survey",
+		"author": "It's Nice That"
+	},
+	{
+		"path": "inuse/023.jpg",
+		"tags": [
+			"Garamon(d/t)"
+		],
+		"title": "Un Fanzine par Mois",
+		"author": "Jules Tirilly"
+	},
+	{
 		"path": "inuse/022.jpg",
 		"tags": [
 			"Zara"
